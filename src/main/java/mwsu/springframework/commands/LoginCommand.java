dd1cb3ab1b56caadc6631067dd92805b8433c20f
washingtonwei <weibingyang@gmail.com>
2017-05-25 10:01:55 -0500

    Seocnd commit

--- src/main/java/mwsu/springframework/commands/LoginCommand.java
+++ src/main/java/mwsu/springframework/commands/LoginCommand.java
@@ -1,7 +1,31 @@
 package mwsu.springframework.commands;
 
+import org.hibernate.validator.constraints.NotEmpty;
+
 /**
  * Created by bingyang.wei on 5/25/2017.
  */
 public class LoginCommand {
+
+    @NotEmpty
+    private String username;
+
+    @NotEmpty
+    private String password;
+
+    public String getUsername() {
+        return username;
+    }
+
+    public void setUsername(String username) {
+        this.username = username;
+    }
+
+    public String getPassword() {
+        return password;
+    }
+
+    public void setPassword(String password) {
+        this.password = password;
+    }
 }
--- src/main/java/mwsu/springframework/controllers/LoginController.java
+++ src/main/java/mwsu/springframework/controllers/LoginController.java
@@ -1,7 +1,33 @@
 package mwsu.springframework.controllers;
 
+import mwsu.springframework.commands.LoginCommand;
+import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
+import org.springframework.validation.BindingResult;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+
+import javax.validation.Valid;
+
 /**
  * Created by bingyang.wei on 5/25/2017.
  */
+@Controller
 public class LoginController {
+
+    @RequestMapping("/login")
+    public String checkoutForm(Model model){
+        model.addAttribute("loginCommand", new LoginCommand());
+        return "loginform";
+    }
+    @RequestMapping(value="/dologin", method = RequestMethod.POST)
+    public String doCheckout(@Valid LoginCommand loginCommand, BindingResult bindingResult){
+        if(bindingResult.hasErrors())
+        {
+            return "loginform";
+        }
+
+        return "logincomplete";
+
+    }
 }
