aaa3ce1a14b5546b60730b31960642e818f2a655
washingtonwei <weibingyang@gmail.com>
2017-05-25 10:01:24 -0500

    Seocnd commit

---
+++ src/main/java/mwsu/springframework/commands/LoginCommand.java
@@ -0,0 +1,7 @@
+package mwsu.springframework.commands;
+
+/**
+ * Created by bingyang.wei on 5/25/2017.
+ */
+public class LoginCommand {
+}
---
+++ src/main/java/mwsu/springframework/controllers/LoginController.java
@@ -0,0 +1,7 @@
+package mwsu.springframework.controllers;
+
+/**
+ * Created by bingyang.wei on 5/25/2017.
+ */
+public class LoginController {
+}
